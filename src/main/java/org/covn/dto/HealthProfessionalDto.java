9054458f932d21066723c3e2e4e79977d4c57a5e
Senthil Nachimuthu <sencode@users.noreply.github.com>
2020-06-04 10:35:35 -0600

    Revert "Modifield fields in dto.HelathProfessionalDto.java."
    This reverts commit 946a41c0d5560e49f0510e61b06ee9d295f69ccf.

--- src/main/java/org/covn/dto/HealthProfessionalDto.java
+++ src/main/java/org/covn/dto/HealthProfessionalDto.java
@@ -4,11 +4,6 @@ import java.util.Date;
 
 public class HealthProfessionalDto {
 	
-	// TODO - this first set of fields need to be updated to be the same as in RegisterDto.java
-	// [Senthil's question]:
-	// what is the best way to reuse what's defined in RegisterDto.java?
-	// also, how to use the same service logic for both health professional and patient registration for these common fields?
-	
 	private String title;
 	private String firstName;
 	private String lastName;
@@ -17,17 +12,89 @@ public class HealthProfessionalDto {
 	private Date dateOfBirth;
 	private String emailAddress;
 	private String phoneNumber;
-	
-	// this second set of fields are unique to health professionals. these are in addition to common fields in RegisterDto.java
-	
-	private int healthProJobId; // A single health professional may have multiple jobs. 
-	// Start with 1 for the first job of each person and then increment automatically 
+	private String healthProType;
 	private String healthProJobTitle;
-	private int healthProWorkLocationId;
+	private String healthProOfficeAddress;
+	private int healthProLocationId;
 	private int supervisorId;
-	private int isActive; // default true. set to false if this person doesn't do this job anymore instead of deleting the row
-	
-	// TODO - add getters and setters
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSuffix() {
+		return suffix;
+	}
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getHealthProType() {
+		return healthProType;
+	}
+	public void setHealthProType(String healthProType) {
+		this.healthProType = healthProType;
+	}
+	public String getHealthProJobTitle() {
+		return healthProJobTitle;
+	}
+	public void setHealthProJobTitle(String healthProJobTitle) {
+		this.healthProJobTitle = healthProJobTitle;
+	}
+	public String getHealthProOfficeAddress() {
+		return healthProOfficeAddress;
+	}
+	public void setHealthProOfficeAddress(String healthProOfficeAddress) {
+		this.healthProOfficeAddress = healthProOfficeAddress;
+	}
+	public int getHealthProLocationId() {
+		return healthProLocationId;
+	}
+	public void setHealthProLocationId(int healthProLocationId) {
+		this.healthProLocationId = healthProLocationId;
+	}
+	public int getSupervisorId() {
+		return supervisorId;
+	}
+	public void setSupervisorId(int supervisorId) {
+		this.supervisorId = supervisorId;
+	}
 	
 	
 	
